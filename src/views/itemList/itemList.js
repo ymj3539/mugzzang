dc7683660ff63abc35118472b3be918bc2a727aa
junseolee <dlwnstjzld@gmail.com>
2022-05-24 23:25:19 +0900

    카테고리, 무한스크롤 기능 코드 정리 및 주석 추가

--- src/views/itemList/itemList.js
+++ src/views/itemList/itemList.js
@@ -7,14 +7,21 @@ let flag = null;
 // 데이터 불러오기
 const res = await fetch('./data.json');
 const itemList = await res.json();
+
+// 상품 화면에 띄우는 함수
 const showContent = (index, category) => {
   $article = document.querySelectorAll('#itemlist');
-  let $itemListFlexbox = document.querySelector('.itemlist_flexbox');
+  $itemListFlexbox = document.querySelector('.itemlist_flexbox');
+  // 화면에 상품 띄우고 싶은 만큼 data에서 slice하기
   let sliceItem = itemList.slice($article.length, $article.length + index);
+
+  // category값이 true일 경우, category 값으로 data 필터링
   if (category) {
     const categorizingItem = itemList.filter((e) => e.category === category);
     sliceItem = categorizingItem;
   }
+
+  // 상품 목록 개수와 추출한 데이터 개수가 일치한다면 이스케이프
   if ($article.length === sliceItem.length) return;
   sliceItem.forEach((e) => {
     const {oid, brand, name, price, img, category} = e;
@@ -36,13 +43,20 @@ const showContent = (index, category) => {
   });
 };
 
+// 카테고리 버튼 이벤트리스너 콜백함수
 const showCategoryItem = (e) => {
+  // button 태그의 dataset(category 값과 동일함)을 flag 변수로 이용
   flag = Number(e.target.parentNode.dataset.num);
+  // 기존 상품 목록 노드 전부 지우고 재생성
   $itemListFlexbox = document.querySelector('.itemlist_flexbox');
   $itemListFlexbox.remove();
   const flexbox = document.createElement('div');
   flexbox.className = 'itemlist_flexbox';
   $itemSection.appendChild(flexbox);
+
+  // 변화한다는 효과를 주기 위해 일단 비동기 처리하였음
+  // showContent 함수로 카테고리에 맞는 상품 띄우기
+  // 기존 노드가 삭제됨으로서 intersectionObserver가 관찰하고 있는 관찰 대상이 없음. 다시 관찰 대상 추가해주기
   setTimeout(() => {
     showContent(8, flag); // button 태그의 dataset
     observeLastItem(io, document.querySelectorAll('#itemlist'));
@@ -50,22 +64,17 @@ const showCategoryItem = (e) => {
   }, 100);
 };
 
-showContent(8);
-
 // intersectionObserver 콜백함수
 const ioCallback = (entries, io) => {
-  console.log('check');
-  let scrollId;
   $article = document.querySelectorAll('#itemlist');
   const {isIntersecting, target} = entries[0];
-  // isIntersecting이 진행중인 경우 observing 해제
-  // 더 추가될 컨텐츠가 있는 경우, 마지막 상품 dom모델을 기준으로 observing 시작
   if (isIntersecting) {
-    io.unobserve(target);
+    io.unobserve(target); // 기존 관찰 대상을 취소해줘야 스크롤 했을 때 콜백함수가 생성됨 관찰 대상이 유지된다면 스크롤 안해도 계속 콜백함수가 실행됨.
     setTimeout(() => {
+      // 상품이 추가된다는 느낌을 주고 싶어서 setTimeout 처리하였음
       showContent(4, flag);
       observeLastItem(io, $article);
-    }, 800);
+    }, 700);
   }
 };
 
@@ -75,9 +84,8 @@ const observeLastItem = (io, items) => {
   io.observe(lastItem);
 };
 
+showContent(8); // 랜딩했을 때 최초 상품 8개 띄우기
 const io = new IntersectionObserver(ioCallback, {threshold: 0.9});
-
 observeLastItem(io, document.querySelectorAll('#itemlist'));
-
 $category1.addEventListener('click', showCategoryItem);
 $category2.addEventListener('click', showCategoryItem);
--- src/views/itemList/showCategoryItem.js
+++
@@ -1,4 +0,0 @@
-const $category1 = document.querySelector('#category_1');
-const $category2 = document.querySelector('#category_2');
-
-// $category1.addEventListener('click', '');
