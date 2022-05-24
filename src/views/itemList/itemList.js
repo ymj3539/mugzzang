990baa99e8305a22ffef864c3161d40181859dfb
junseolee <dlwnstjzld@gmail.com>
2022-05-24 23:08:42 +0900

    카테고리 기능 구현 완료

--- src/views/itemList/itemList.js
+++ src/views/itemList/itemList.js
@@ -1,77 +1,83 @@
-const $content = document.querySelector('#content');
-const $flexbox = document.querySelector('.itemlist_flexbox');
+let $itemListFlexbox = document.querySelector('.itemlist_flexbox');
+const $itemSection = document.querySelector('#itemlist_section');
+const $category1 = document.querySelector('#category_1');
+const $category2 = document.querySelector('#category_2');
 let $article = document.querySelectorAll('#itemlist');
-// 임시 데이터 불러오기
+let flag = null;
+// 데이터 불러오기
 const res = await fetch('./data.json');
 const itemList = await res.json();
-
-// 최초에 보여질 8개 상품 반복문으로 추가
-for (let i = 0; i <= 7; i++) {
-  const {oid, brand, name, price, img} = itemList[i];
-  $flexbox.insertAdjacentHTML(
-    'beforeend',
-    `<article data-oid=${oid} id="itemlist" class="itemlist">
-      <a href="#">
-      <div>
-        <img src=${img} alt="itemImg" />
-        <div>
-          <h2>${brand}</h2>
-          <h2>${name}</h2>
-          <p>${price}</p>
-        </div>
-      </div>
-      </a>
-    </article>`
-  );
-}
-
-// 스크롤이 일정 부분까지 진행됐을 때 추가할 컨텐츠
-const addNewContent = () => {
-  console.log('실행');
-  let $article = document.querySelectorAll('#itemlist');
-  for (let i = 0; i < 4; i++) {
-    // 더 추가될 컨텐츠가 없는 경우(false인 경우), for문 break
-    if (itemList[$article.length + i]) {
-      const {oid, brand, name, price, img} = itemList[$article.length + i];
-      $flexbox.insertAdjacentHTML(
-        'beforeend',
-        `<article data-oid=${oid} id="itemlist" class="itemlist">
-      <a href="#">
-      <div>
-        <img src=${img} alt="itemImg" />
-        <div>
-          <h2>${brand}</h2>
-          <h2>${name}</h2>
-          <p>${price}</p>
-        </div>
-      </div>
-      </a>
-    </article>`
-      );
-    } else break;
+const showContent = (index, category) => {
+  $article = document.querySelectorAll('#itemlist');
+  let $itemListFlexbox = document.querySelector('.itemlist_flexbox');
+  let sliceItem = itemList.slice($article.length, $article.length + index);
+  if (category) {
+    const categorizingItem = itemList.filter((e) => e.category === category);
+    sliceItem = categorizingItem;
   }
+  if ($article.length === sliceItem.length) return;
+  sliceItem.forEach((e) => {
+    const {oid, brand, name, price, img, category} = e;
+    $itemListFlexbox.insertAdjacentHTML(
+      'beforeend',
+      `<article data-oid=${oid} data-cg=${category} id="itemlist" class="itemlist">
+      <a href="#">
+      <div>
+        <img src=${img} alt="itemImg" />
+        <div>
+          <h2>${brand}</h2>
+          <h2>${name}</h2>
+          <p>${price}</p>
+        </div>
+      </div>
+      </a>
+    </article>`
+    );
+  });
 };
+
+const showCategoryItem = (e) => {
+  flag = Number(e.target.parentNode.dataset.num);
+  $itemListFlexbox = document.querySelector('.itemlist_flexbox');
+  $itemListFlexbox.remove();
+  const flexbox = document.createElement('div');
+  flexbox.className = 'itemlist_flexbox';
+  $itemSection.appendChild(flexbox);
+  setTimeout(() => {
+    showContent(8, flag); // button 태그의 dataset
+    observeLastItem(io, document.querySelectorAll('#itemlist'));
+    console.log('실행');
+  }, 100);
+};
+
+showContent(8);
 
 // intersectionObserver 콜백함수
 const ioCallback = (entries, io) => {
+  console.log('check');
+  let scrollId;
   $article = document.querySelectorAll('#itemlist');
   const {isIntersecting, target} = entries[0];
-  // isIntersecting이 진행중인 경우 observering 해제
+  // isIntersecting이 진행중인 경우 observing 해제
+  // 더 추가될 컨텐츠가 있는 경우, 마지막 상품 dom모델을 기준으로 observing 시작
   if (isIntersecting) {
     io.unobserve(target);
-  }
-  // 더 추가될 컨텐츠가 있는 경우, 마지막 상품 dom모델을 기준으로 observing 시작
-  if ($article.length !== itemList.length) {
     setTimeout(() => {
-      addNewContent();
+      showContent(4, flag);
       observeLastItem(io, $article);
-    }, 2000);
+    }, 800);
   }
 };
 
+// 관찰 대상 상품 리스트 마지막 요소로 변경
 const observeLastItem = (io, items) => {
   let lastItem = items[items.length - 1];
   io.observe(lastItem);
 };
-const io = new IntersectionObserver(ioCallback, {threshold: 0.7});
+
+const io = new IntersectionObserver(ioCallback, {threshold: 0.9});
+
 observeLastItem(io, document.querySelectorAll('#itemlist'));
+
+$category1.addEventListener('click', showCategoryItem);
+$category2.addEventListener('click', showCategoryItem);
