9ec87029e0cbaff8c38ee0093ae41403bd2f7b30
[SW2/서울/2반] 이준서 <dlwnstjzld@gmail.com>
2022-05-25 08:20:48 +0000

    Revert "Merge branch 'F/itemList' into 'main'"
    This reverts merge request !1

--- src/views/login/login.js
+++ src/views/login/login.js
@@ -1,5 +1,5 @@
 import * as Api from '/api.js';
-import {validateEmail} from '/useful-functions.js';
+import { validateEmail } from '/useful-functions.js';
 
 // 요소(element), input 혹은 상수
 const emailInput = document.querySelector('#emailInput');
@@ -29,12 +29,14 @@ async function handleSubmit(e) {
   const isPasswordValid = password.length >= 4;
 
   if (!isEmailValid || !isPasswordValid) {
-    return alert('비밀번호가 4글자 이상인지, 이메일 형태가 맞는지 확인해 주세요.');
+    return alert(
+      '비밀번호가 4글자 이상인지, 이메일 형태가 맞는지 확인해 주세요.'
+    );
   }
 
   // 로그인 api 요청
   try {
-    const data = {email, password};
+    const data = { email, password };
 
     const result = await Api.post('/api/login', data);
     const token = result.token;
@@ -42,7 +44,7 @@ async function handleSubmit(e) {
     // 로그인 성공, 토큰을 세션 스토리지에 저장
     // 물론 다른 스토리지여도 됨
     sessionStorage.setItem('token', token);
-    sessionStorage.setItem('id', email);
+
     alert(`정상적으로 로그인되었습니다.`);
 
     // 로그인 성공
