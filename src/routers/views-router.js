e71ee238795a642dcb6d190bff28bb7be4a1e8d6
junseolee <dlwnstjzld@gmail.com>
2022-05-25 16:38:32 +0900

    상세 페이지 목 데이터로 구현 완료

--- src/routers/views-router.js
+++ src/routers/views-router.js
@@ -10,6 +10,7 @@ viewsRouter.use('/', serveStatic('home'));
 viewsRouter.use('/register', serveStatic('register'));
 viewsRouter.use('/login', serveStatic('login'));
 viewsRouter.use('/itemList', serveStatic('itemList'));
+viewsRouter.use('/itemInfo', serveStatic('itemInfo'));
 
 // views 폴더의 최상단 파일인 rabbit.png, api.js 등을 쓸 수 있게 함
 viewsRouter.use('/', serveStatic(''));
