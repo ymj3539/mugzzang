15a6e10f83f2464c31dd9e380d0327e66af0f558
cofla <cofla159@naver.com>
2022-05-26 06:16:44 +0900

    장바구니 목록 구현

--- src/views/cart/cart.js
+++ src/views/cart/cart.js
@@ -2,11 +2,12 @@
 import {quantityControlBox} from './quantityControlBox.js'; 
 
 const cartList = document.querySelector('.cartList');
+const payInfo = document.querySelector('.payInfo');
 
 //장바구니 리스트 불러오기
 async function getCartList() {
     try {
-        const res = await fetch('./tempData1_cartList.json');
+        const res = await fetch('./tempData_cartList.json');
         const fetchedCartList = await res.json();
         return fetchedCartList;
     } catch(err) {
@@ -16,7 +17,7 @@ async function getCartList() {
 //제품 목록 리스트 불러오기
 async function getitemList() {
     try {
-        const res = await fetch('./tempData2_itemList.json');
+        const res = await fetch('./tempData_itemList.json');
         const fetchedItemList = await res.json();
         return fetchedItemList;
     } catch(err) {
@@ -28,12 +29,31 @@ let inCartList = await getCartList();
 if(inCartList.length < 1) {
     cartList.innerHTML = `장바구니가 비어 있습니다:(`;
 } else {
-    let itemList =await getitemList();
+    let itemList = await getitemList();
     inCartList = inCartList.slice(2).map(x => Object.values(x)).flat();
-    console.log(inCartList);
-    inCartList.forEach((cart) => {
-        cartList.innerHTML += `<div>상품아이디: ${cart.id}     수량: ${cart.quantity}</div>`
-    })
+    console.log(itemList);
     
+    inCartList.forEach((cart) => {
+        const foundItem = itemList.find(item => {
+            if(item.oid == cart.id) {
+                let itemName = item.name;
+                let itemPrice = item.price;
+                let itemImg = item.img;
+                return {itemName, itemPrice, itemImg};
+            }
+        })
+        const totalItemPrice = parseInt(foundItem.price) * (cart.quantity) * 1000;
+
+        cartList.innerHTML += `
+        <div clas="item">
+            <a href="#">
+                <img class="itemImage" src="${foundItem.img}"/>
+                <span class="itemName">${foundItem.name}</span>
+                <span class="itemQuantity">${cart.quantity}</span>
+                <span class="itemPrice">${foundItem.price}</span>
+                <span class="totalItemPrice">${totalItemPrice}</span>
+            </a>
+        </div>`
+    }) 
 }
     
