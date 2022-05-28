ab3444fdaa4fbb8727ca27d7ea65581bf068b9d1
WinterKwon <winterkwon7017@gmail.com>
2022-05-28 10:53:27 +0900

    주문 api 중 모델 작성

---
+++ src/db/models/order-model.js
@@ -0,0 +1,38 @@
+import {model} from 'mongoose';
+import { OrderSchema } from '../schemas/order-schema';
+const Order = model('orders', OrderSchema);
+
+export class OrderModel {
+    async findById(orderId) {
+        const order = await Order.findOne({ _id: orderId });
+        return order;
+      }
+    
+    async update({useremail, update}){
+        const filter = {email : useremail};
+        const option = {returnOriginal : false};
+
+        const updatedOrder = await Order.findOneAndUpdate(filter, update, option);
+
+        return updatedOrder;
+    } 
+
+    async delete(orderId) {
+        await Order.findOneAndDelete({_id : orderId})
+    }  
+
+    //미사용시 삭제하기
+    async findAll(){
+        const orders = await Order.find({});
+        return orders;
+    }
+
+    async create (orderinfo) {
+        const newOrder = await Order.create(orderinfo);
+        return newOrder
+    }
+}
+
+const orderModel = new OrderModel();
+
+export {orderModel};
