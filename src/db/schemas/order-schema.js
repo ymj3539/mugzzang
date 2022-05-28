52d385fbb3c51b89c0e4ada1254c39bb80090e0f
WinterKwon <winterkwon7017@gmail.com>
2022-05-28 10:53:27 +0900

    주문 api 중 모델 작성

---
+++ src/db/schemas/order-schema.js
@@ -0,0 +1,54 @@
+import {Schema} from 'mongoose';
+import {shortId} from './types/short-id';
+
+const OrderSchema = new Schema({
+    shortId, 
+    email : {
+        type : String,
+        required : true
+    },
+
+    price : [{
+        price_each : {
+            type : Number,
+            required : true
+        },
+        count : {
+            type : Number,
+            required : true
+        },
+
+        total : {
+            type : Number,
+            required : true
+        }
+    }],
+
+    delivery : [{
+        name : {
+            type : String,
+            required : true
+        },
+        contact : {
+            type : [String],
+            required : true
+        },
+        address : {
+            type: String,
+            required : true
+        },
+
+        
+    }],
+
+    orderNumber : {
+        type : String,
+        required : false
+    }
+
+}, {
+    collection: 'orders',
+    timestamps: true,
+  });
+
+  export {OrderSchema};
