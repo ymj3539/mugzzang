77126aca4b39e9526d2bb6f96f94da2a8e392fc8
WinterKwon <winterkwon7017@gmail.com>
2022-05-29 23:57:50 +0900

    관리자 확인  미들웨어 구현

---
+++ src/middlewares/admin-required.js
@@ -0,0 +1,29 @@
+import jwt from 'jsonwebtoken';
+
+const adminRequired = (req, res, next) => {
+    const userToken = req.headers['authorization']?.split(' ')[1];
+
+    try {
+        const secretKey = process.env.JWT_SECRET_KEY || 'secret-key';
+        const jwtDecoded = jwt.verify(userToken, secretKey);
+        const userRole = jwtDecoded.role;
+
+        if(userRole !== "admin"){
+            res.status(403).json({
+                result : "forbidden-approach",
+                reason : '접근 권한이 없는 페이지입니다.'
+            });
+            return;
+        }
+
+        next();
+
+
+
+    } catch (error){
+        next(error);
+    }
+    
+ }
+
+export {adminRequired};
