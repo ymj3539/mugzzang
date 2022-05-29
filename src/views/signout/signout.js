87197c1ecef992628b0bd235171e63742cfb2236
[SW2/서울/2반]성경주 <yesican1011@naver.com>
2022-05-30 02:48:05 +0900

    회원정보 수정기능 완료

--- src/views/signout/signout.js
+++ src/views/signout/signout.js
@@ -1,5 +1,4 @@
 import * as Api from "/api.js";
-import { randomId } from "/useful-functions.js";
 
 // 요소(element), input 혹은 상수
 const logoutBtn = document.querySelector("#logoutBtn");
@@ -28,17 +27,26 @@ function logout() {
   }
 }
 
+//계정삭제
 async function signout() {
-  //   e.preventDefault();
   const password = passwordInput.value;
   const data = { password };
 
-  if (confirm("정말 삭제하시겠습니까?") == true) {
-    await Api.delete("/api/userlist", sessionStorage.getItem("id"), data);
-    sessionStorage.clear();
-    alert("계정이 삭제되었습니다.");
-    window.location.href = "/";
-  } else {
-    return false;
+  try {
+    if (confirm("정말 삭제하시겠습니까?") == true) {
+      await Api.delete(
+        "/api/user/userlist",
+        sessionStorage.getItem("id"),
+        data
+      );
+      sessionStorage.clear();
+      alert("계정이 삭제되었습니다.");
+      window.location.href = "/";
+    } else {
+      return false;
+    }
+  } catch (err) {
+    console.error(err.stack);
+    alert(`문제가 발생하였습니다. 확인 후 다시 시도해 주세요: ${err.message}`);
   }
 }
