4be7390db64ea06a8d2fed1eecfc58487d634a3f
[SW2/서울/2반]성경주 <yesican1011@naver.com>
2022-05-31 18:53:38 +0900

    회원정보 수정, 삭제 오류해결

--- src/views/signout/signout.js
+++ src/views/signout/signout.js
@@ -1,7 +1,6 @@
 import * as Api from "/api.js";
 
 // 요소(element), input 혹은 상수
-const logoutBtn = document.querySelector("#logoutBtn");
 const deleteBtn = document.querySelector("#deleteBtn");
 const passwordInput = document.querySelector("#passwordInput");
 
@@ -13,18 +12,7 @@ async function addAllElements() {}
 
 // 여러 개의 addEventListener들을 묶어주어서 코드를 깔끔하게 하는 역할임.
 function addAllEvents() {
-  logoutBtn.addEventListener("click", logout);
   deleteBtn.addEventListener("click", signout);
-}
-
-function logout() {
-  //로그아웃 버튼 클릭시 세션스토리지 삭제
-  if (sessionStorage.getItem("token")) {
-    console.log(sessionStorage.getItem("id"));
-    sessionStorage.clear();
-    alert("로그아웃 하였습니다.");
-    window.location.href = "/";
-  }
 }
 
 //계정삭제
