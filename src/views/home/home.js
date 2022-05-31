043ccff43184b8aadc68d78e743dab853d43db94
[SW2/서울/2반]성경주 <yesican1011@naver.com>
2022-05-31 18:41:05 +0900

    header 컴포넌트

--- src/views/home/home.js
+++ src/views/home/home.js
@@ -16,30 +16,3 @@ function addAllEvents() {
 }
 
 //메인 배너
-var slider = document.querySelector("#slider");
-var slides = slider.querySelector(".slides");
-var slide = slides.querySelectorAll(".slide");
-var leftButton = document.querySelector(".sliderArrowLeft");
-var rightButton = document.querySelector(".sliderArrowRight");
-
-var currentSlide = 0;
-
-setInterval(function () {
-  var from = -(1024 * currentSlide);
-  var to = from - 1024;
-  slides.animate(
-    {
-      marginLeft: [from + "px", to + "px"],
-    },
-    {
-      duration: 500,
-      easing: "ease",
-      iterations: 1,
-      fill: "both",
-    }
-  );
-  currentSlide++;
-  if (currentSlide === slide.length - 1) {
-    currentSlide = 0;
-  }
-}, 3000);
