6c6c917e140cabc5876e00f8a14b05849efdfe1d
[SW2/서울/2반]성경주 <yesican1011@naver.com>
2022-06-01 17:00:26 +0900

    작은 오류 수정

--- src/views/mypage/mypage.js
+++ src/views/mypage/mypage.js
@@ -9,12 +9,10 @@ const userPhonenum = document.querySelector("#userPhonenum");
 addAllElements();
 addAllEvents();
 
-// html에 요소를 추가하는 함수들을 묶어주어서 코드를 깔끔하게 하는 역할임.
 async function addAllElements() {
   mypageInfo();
 }
 
-// 여러 개의 addEventListener들을 묶어주어서 코드를 깔끔하게 하는 역할임.
 function addAllEvents() {}
 
 //마이페이지에 쓰일 로그인 유저의 정보 get (세션 스토리지값을 이용)
