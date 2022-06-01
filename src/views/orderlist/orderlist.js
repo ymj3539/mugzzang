c6fce8b57ffc015de30b75d2e7b0ef3239da399e
[SW2/서울/2반]성경주 <yesican1011@naver.com>
2022-06-01 15:59:27 +0900

    주문조회 열람, 삭제 기능 완료

--- src/views/orderlist/orderlist.js
+++ src/views/orderlist/orderlist.js
@@ -1,6 +1,8 @@
 import * as Api from "/api.js";
 
 // 요소(element), input 혹은 상수
+const orderlistContainer = document.getElementById("orderlist-container");
+// const orderInfo = document.getElementById("orderInfo");
 
 addAllElements();
 addAllEvents();
@@ -18,10 +20,34 @@ async function mypageInfo() {
     alert("로그인한 유저만 사용할 수 있는 서비스입니다.");
     window.location.href = "/login";
   }
-  const resUser = await Api.get(
-    `/api/user/userlist/${sessionStorage.getItem("id")}`
+  const table = document.createElement("div");
+  table.id = "orderInfo";
+  orderlistContainer.appendChild(table);
+
+  const res = await Api.get(`/api/order/${sessionStorage.getItem("id")}`);
+  console.log(res);
+
+  res.map((el) =>
+    table.insertAdjacentHTML(
+      "beforeend",
+      `
+  <div class="orderlist-standard">
+      <span>${el.createdAt.substr(0, 10)}</span>
+      <span>${el.productName}</span>
+      <span>상품 준비중</span>
+      <span><button id='delBtn' data-shortid=${
+        el.shortId
+      }>주문 취소</button></span>
+    </div>`
+    )
   );
-  const resOrder = await Api.get(`/api/order/${sessionStorage.getItem("id")}`);
-  console.log(resUser);
-  console.log(resOrder);
+
+  async function cancelEvent(e) {
+    let target = e.target;
+    if (e.target.id !== "delBtn") return;
+    await Api.delete("/api/order/orderlist", target.dataset.shortid);
+    target.parentNode.parentNode.remove();
+    alert("주문이 취소되었습니다.");
+  }
+  table.addEventListener("click", cancelEvent);
 }
