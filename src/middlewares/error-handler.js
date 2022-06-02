300f65c3287bc44fd6d55efa171f253d92ce7b58
MinJuYoon <ymj3539@naver.com>
2022-06-02 13:07:06 +0900

    error-handler(진행중)

--- src/middlewares/error-handler.js
+++ src/middlewares/error-handler.js
@@ -4,8 +4,16 @@ function errorHandler(error, req, res, next) {
   // 터미널에 노란색으로 출력됨.
   console.log('\x1b[33m%s\x1b[0m', error.stack);
 
+  switch (error.message) {
+    case 'headers의 Content-Type을 application/json으로 설정해주세요':
+      res.status(400).json({
+        status: 400,
+        reason: `headers의 Content-Type을 application/json으로 설정해주세요`,
+      });
+  }
+
   // 에러는 400 코드의 JSON 형태로 프론트에 전달됨
-  res.status(400).json({ result: 'error', reason: error.message });
+  res.status(400).json({ status: 400, result: 'error', reason: error.message });
 }
 
 export { errorHandler };
