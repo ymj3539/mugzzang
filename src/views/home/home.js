420fd938330bb3a273268cc4f5f75d509956bdfc
cofla <cofla159@naver.com>
2022-06-03 18:26:46 +0900

    배너 클릭 시 카테고리로 이동

--- src/views/home/home.js
+++ src/views/home/home.js
@@ -30,3 +30,19 @@ $leftBtn.addEventListener('click', () => {
 $rightBtn.addEventListener('click', () => {
 
 });
+
+const $categoryBig = document.getElementById("categoryBig");
+const $categorySmall = document.getElementById("categorySmall");
+$categoryBig.addEventListener("click", categoryBtn);
+$categorySmall.addEventListener("click", categoryBtn);
+
+function categoryBtn(e) {
+  console.log(e.target);
+  if (e.target.innerHTML === "도매") {
+    sessionStorage.setItem("chosenCategory", "도매");
+  }
+  if (e.target.innerHTML === "소매") {
+    sessionStorage.setItem("chosenCategory", "소매");
+  }
+  window.location.href = "/itemlist";
+}
