b69876e16662826f67a7def73f301efc092f8d48
junseolee <dlwnstjzld@gmail.com>
2022-06-04 00:14:05 +0900

    체크

--- src/views/header/header.js
+++ src/views/header/header.js
@@ -1,8 +1,8 @@
-import * as Api from "/api.js";
+import * as Api from '/api.js';
 
-const navBar = document.getElementById("navbar");
-const category1 = document.getElementById("category1");
-const category2 = document.getElementById("category2");
+const navBar = document.getElementById('navbar');
+const category1 = document.getElementById('category1');
+const category2 = document.getElementById('category2');
 
 addAllElements();
 addAllEvents();
@@ -13,24 +13,24 @@ async function addAllElements() {
 }
 
 function addAllEvents() {
-  category1.addEventListener("click", categoryBtn);
-  category2.addEventListener("click", categoryBtn);
+  category1.addEventListener('click', categoryBtn);
+  category2.addEventListener('click', categoryBtn);
 }
 
 function loginUser() {
   //세션스토리지에 토큰 유무로 nev에 보여야할 옵션 변경(로그인 상태와 비로그인 상태 구분)
-  if (!sessionStorage.getItem("token")) {
+  if (!sessionStorage.getItem('token')) {
     navBar.insertAdjacentHTML(
-      "beforeend",
+      'beforeend',
       `
         <li class="mainlogo"><a href="/"><span class="logoStyle">MUGZZANG</span></a></li>
         <li><a href="/login">로그인</a></li>
         <li><a href="/register">회원가입</a></li>
         `
     );
-  } else if (sessionStorage.getItem("token")) {
+  } else if (sessionStorage.getItem('token')) {
     navBar.insertAdjacentHTML(
-      "beforeend",
+      'beforeend',
       `
         <li class="mainlogo"><a href="/"><span class="logoStyle">MUGZZANG</span></a></li>
         <li>
@@ -46,28 +46,28 @@ function loginUser() {
 }
 
 function creatLogout() {
-  const logoutbtn = document.createElement("a");
-  logoutbtn.innerHTML = "로그아웃";
-  logoutbtn.id = "logoutBtn";
+  const logoutbtn = document.createElement('a');
+  logoutbtn.innerHTML = '로그아웃';
+  logoutbtn.id = 'logoutBtn';
   logoutbtn.onclick = function () {
-    if (sessionStorage.getItem("token")) {
+    if (sessionStorage.getItem('token')) {
       sessionStorage.clear();
-      alert("로그아웃 하였습니다.");
-      window.location.href = "/";
+      alert('로그아웃 하였습니다.');
+      window.location.href = '/';
     }
   };
-  if (sessionStorage.getItem("id")) {
+  if (sessionStorage.getItem('id')) {
     navBar.appendChild(logoutbtn);
   }
 }
 
 function categoryBtn(event) {
-  if (event.target.innerHTML === "도매") {
-    sessionStorage.setItem("chosenCategory", "도매");
+  if (event.target.innerHTML === '도매') {
+    sessionStorage.setItem('chosenCategory', '도매');
   }
-  if (event.target.innerHTML === "소매") {
-    sessionStorage.setItem("chosenCategory", "소매");
+  if (event.target.innerHTML === '소매') {
+    sessionStorage.setItem('chosenCategory', '소매');
   }
 
-  window.location.href = "/itemlist";
+  window.location.href = '/itemlist';
 }
--- src/views/home/home.js
+++ src/views/home/home.js
@@ -1,15 +1,14 @@
-const $categoryBig = document.getElementById("categoryBig");
-const $categorySmall = document.getElementById("categorySmall");
-$categoryBig.addEventListener("click", categoryBtn);
-$categorySmall.addEventListener("click", categoryBtn);
+const $categoryBig = document.getElementById('categoryBig');
+const $categorySmall = document.getElementById('categorySmall');
+$categoryBig.addEventListener('click', categoryBtn);
+$categorySmall.addEventListener('click', categoryBtn);
 
 function categoryBtn(e) {
-  console.log(e.target);
-  if (e.target.innerHTML === "도매") {
-    sessionStorage.setItem("chosenCategory", "도매");
+  if (e.target.id === 'categoryBig') {
+    sessionStorage.setItem('chosenCategory', '도매');
   }
-  if (e.target.innerHTML === "소매") {
-    sessionStorage.setItem("chosenCategory", "소매");
+  if (e.target.id === 'categorySmall') {
+    sessionStorage.setItem('chosenCategory', '소매');
   }
-  window.location.href = "/itemlist";
+  window.location.href = '/itemlist';
 }
