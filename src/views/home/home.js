11656eb20716cd9801af091308db58ca8564edc8
cofla <cofla159@naver.com>
2022-06-04 06:37:16 +0900

    메인배너 링크 디버깅

--- src/views/home/home.js
+++ src/views/home/home.js
@@ -1,9 +1,10 @@
-const $categoryBig = document.getElementById('categoryBig');
-const $categorySmall = document.getElementById('categorySmall');
-$categoryBig.addEventListener('click', categoryBtn);
-$categorySmall.addEventListener('click', categoryBtn);
+const $whole = document.getElementById('wholePeople');
+const $retail = document.getElementById('retailPeople');
+$whole.addEventListener('click', categoryBtn);
+$retail.addEventListener('click', categoryBtn);
 
 function categoryBtn(e) {
+  console.log(e.target);
   if (e.target.id === 'categoryBig') {
     sessionStorage.setItem('chosenCategory', '도매');
   }
